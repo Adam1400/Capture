88e60f0ef3f1d9a4c4b0e8cee241bcf880f3b4da
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-09-28 13:13:50 -0600

    database works localy
    this does not work on heroku yet. so for right now we are still using dummy data

--- capture/post/urls.py
+++ capture/post/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from .views import PostListView
 from . import views
-
+from .views import PostListView
 
 
 # app routes
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='capture-home'),
+    # not working on heroku
+    # path('', PostListView.as_view(), name='capture-home'),
+    path('', views.index, name='capture-home'),
     path('post/', views.post, name='capture-post'),
 ]
--- capture/post/views.py
+++ capture/post/views.py
@@ -3,13 +3,36 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post
 
-
-
+# temporary dummy data
+posts = [
+    {
+        'user': 'adam1400',
+        'comment': 'Testing database... works localy. Does not work on heroku YET. Striggling to get staic files hosted on heroko. This includes css and in this case database migrations.',
+        'content': 'https://arts.unco.edu/images/music/campus-commons/CCPH_Interior_1200x800.jpg',
+        'post_date': 'September 28, 2020'    
+    },
+    {
+        'user': 'adam1400',
+        'comment': 'testing test 1, comments would go here',
+        'content': 'https://pbs.twimg.com/media/DeDR1NgW0AEgDJq.jpg',
+        'post_date': 'September 19, 2020'
+    },
+    {
+        'user': 'adam1400',
+        'comment': 'second post test 2, comments would go here',
+        'content': 'https://patch.com/img/cdn20/users/22137850/20200907/043945/styles/patch_image/public/screen-shot-2020-09-07-at-10627-pm___07163411370.png?width=695',
+        'post_date': 'September 9, 2020'       
+    }   
+]
 
 
 # load templates
 def index(request):
-    context = { 'posts': Post.objects.all() }
+    # this is not working on heroku
+    # context = { 'posts': Post.objects.all() }
+    context = {
+        'posts': posts
+    }
     return render(request, 'post/index.html', context)
 
 class PostListView(ListView):
