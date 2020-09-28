a0e80cc223ffa544cf30b01253222886aa435309
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-09-28 13:18:40 -0600

    made it so we can experiment localy with database
    (dont push this to heroku)

--- capture/post/models.py
+++ capture/post/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-<<<<<<< Updated upstream
 from django.utils import timezone
 from django.contrib.auth.models import User
 
@@ -11,22 +10,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.comment
-=======
-from django.contrib.auth.models import User
-
-
-class Author(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.pk}. {self.name} {self.user.email}'
-
-
-class Picture(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
-    title = models.CharField(max_length=100)
-
-    def __str__(self):
-        return f'{self.pk}. {self.picName} by {self.author.name}'
->>>>>>> Stashed changes
--- capture/post/urls.py
+++ capture/post/urls.py
@@ -7,7 +7,7 @@ from .views import PostListView
 
 urlpatterns = [
     # not working on heroku
-    # path('', PostListView.as_view(), name='capture-home'),
-    path('', views.index, name='capture-home'),
+    path('', PostListView.as_view(), name='capture-home'),
+    #path('', views.index, name='capture-home'),
     path('post/', views.post, name='capture-post'),
 ]
