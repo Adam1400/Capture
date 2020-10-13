8748c331a1ce0fae471a60b48697cfa4e7d9fb82
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-10-12 20:30:08 -0600

    made anon default user

--- capture/post/models.py
+++ capture/post/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 class Post(models.Model):
-    user = models.ForeignKey(User, unique=False, on_delete=models.CASCADE, default='0001')
+    user = models.ForeignKey(User, unique=False, on_delete=models.CASCADE, default='0002')
     comment = models.TextField()
     content = models.URLField()
     post_date = models.DateTimeField(default=timezone.now)
