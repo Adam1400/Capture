bd214fed825314810073f707657bc94420962d3f
Adam1400 <42878557+Adam1400@users.noreply.github.com>
2020-10-19 16:23:30 -0600

    added profile pics

--- capture/users/views.py
+++ capture/users/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
+from .models import Profile
+from django.forms import ModelForm
+from django.db import models
+from django.contrib.auth.models import User
+from django import forms
+
 
 # Registers an account with a username, password, and email address
 def register(request):
@@ -18,7 +24,22 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    if request.method == 'POST':
+        form = changePic(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Your Profile Picture has been updated!')
+            return redirect('profile')
+    
+    else: 
+        form = changePic(instance=request.user.profile)
+        
+    return render(request, 'users/profile.html', {'form': form})
+
+class changePic(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
 
 
 
